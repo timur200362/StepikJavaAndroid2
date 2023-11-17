c6dc526d92c4a5460138eef6614c1be4df4b6cc1
timur200362 <kh-g-2016@mail.ru>
2023-11-17 05:59:06 +0300

    refactoring-viewModel+RxJava(9.5 lesson)

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -4,56 +4,25 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    private static final String TAG = "MainActivity";
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadDogImage();
-    }
-
-    private void loadDogImage() {
-        new Thread(new Runnable() {
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadDogImage();
+        viewModel.getDogImage().observe(this, new Observer<DogImage>() {
             @Override
-            public void run() {
-                try {
-                    URL url = new URL(BASE_URL);//создаем адрес для отправки запроса
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();//открываем соединение и сохраняем его
-                    InputStream inputStream = urlConnection.getInputStream();//чтобы считывать данные с сервера(считывает по байтам)
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);//чтобы считывать по символам
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);//чтобы считывать единой строкой
-
-                    StringBuilder data = new StringBuilder();//для оптимизации StringBuilder
-                    String result;
-                    do {//цикл чтобы считывать много строк, а не одну
-                        result = bufferedReader.readLine();
-                        if (result != null) {
-                            data.append(result);
-                        }
-                    } while (result != null);
-
-                    JSONObject jsonObject=new JSONObject(data.toString());
-                    String message=jsonObject.getString("message");
-                    String status=jsonObject.getString("status");
-                    DogImage dogImage=new DogImage(message, status);
-
-                    Log.d("MainActivity", dogImage.toString());
-                } catch (Exception e) {
-                    Log.d("MainActivity", e.toString());
-                }
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
             }
-        }).start();
+        });
     }
 }
---
+++ app/src/main/java/com/example/dogs/MainViewModel.java
@@ -0,0 +1,94 @@
+package com.example.dogs;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Callable;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainViewModel extends AndroidViewModel {
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    private static final String KEY_MESSAGE = "message";
+    private static final String KEY_STATUS = "status";
+    private static final String TAG = "MainViewModel";
+
+    private MutableLiveData<DogImage> dogImage = new MutableLiveData<>();
+    private CompositeDisposable compositeDisposable=new CompositeDisposable();
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<DogImage> getDogImage() {
+        return dogImage;
+    }
+
+    public void loadDogImage() {
+        Disposable disposable=loadDogImageRx()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<DogImage>() {
+                    @Override
+                    public void accept(DogImage image) throws Throwable {
+                        dogImage.setValue(image);
+                    }
+                }, new Consumer<Throwable>(){
+            @Override
+            public void accept(Throwable throwable) throws Throwable {
+                Log.d(TAG,"Error: " + throwable.getMessage());
+            }
+        });
+        compositeDisposable.add(disposable);
+    }
+
+    private Single<DogImage> loadDogImageRx() {
+        return Single.fromCallable(new Callable<DogImage>() {
+            @Override
+            public DogImage call() throws Exception {
+                URL url = new URL(BASE_URL);//создаем адрес для отправки запроса
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();//открываем соединение и сохраняем его
+                InputStream inputStream = urlConnection.getInputStream();//чтобы считывать данные с сервера(считывает по байтам)
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);//чтобы считывать по символам
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);//чтобы считывать единой строкой
+
+                StringBuilder data = new StringBuilder();//для оптимизации StringBuilder
+                String result;
+                do {//цикл чтобы считывать много строк, а не одну
+                    result = bufferedReader.readLine();
+                    if (result != null) {
+                        data.append(result);
+                    }
+                } while (result != null);
+
+                JSONObject jsonObject = new JSONObject(data.toString());
+                String message = jsonObject.getString(KEY_MESSAGE);
+                String status = jsonObject.getString(KEY_STATUS);
+                return new DogImage(message, status);
+            }
+        });
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
+    }
+}
