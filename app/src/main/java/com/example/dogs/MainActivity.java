3870bdad2d2a8ed4b00d8955483fa1aa1975ac08
timur200362 <kh-g-2016@mail.ru>
2023-11-17 05:23:52 +0300

    stringBuilder

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -1,22 +1,19 @@
 package com.example.dogs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL="https://dog.ceo/api/breeds/image/random";
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loadDogImage();
     }
-    private void loadDogImage(){
+
+    private void loadDogImage() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL url=new URL(BASE_URL);//создаем адрес для отправки запроса
-                    HttpURLConnection urlConnection=(HttpURLConnection) url.openConnection();//открываем соединение и сохраняем его
-                    InputStream inputStream=urlConnection.getInputStream();//чтобы считывать данные с сервера(считывает по байтам)
-                    InputStreamReader inputStreamReader=new InputStreamReader(inputStream);//чтобы считывать по символам
-                    BufferedReader bufferedReader=new BufferedReader(inputStreamReader);//чтобы считывать единой строкой
-                    String result=bufferedReader.readLine();
-                    Log.d("MainActivity", result);
+                    URL url = new URL(BASE_URL);//создаем адрес для отправки запроса
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();//открываем соединение и сохраняем его
+                    InputStream inputStream = urlConnection.getInputStream();//чтобы считывать данные с сервера(считывает по байтам)
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);//чтобы считывать по символам
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);//чтобы считывать единой строкой
+
+                    StringBuilder data = new StringBuilder();//для оптимизации StringBuilder
+                    String result;
+                    do {//цикл чтобы считывать много строк, а не одну
+                        result = bufferedReader.readLine();
+                        if (result != null) {
+                            data.append(result);
+                        }
+                    } while (result != null);
+                    Log.d("MainActivity", data.toString());
                 } catch (Exception e) {
                     Log.d("MainActivity", e.toString());
                 }
