35ff3139100df4bebd5f147461317bd1dc2fd70a
timur200362 <kh-g-2016@mail.ru>
2023-11-17 06:15:54 +0300

    Glide(9.6 lesson)

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -2,27 +2,45 @@ package com.example.dogs;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private ImageView imageViewDog;
+    private ProgressBar progressBar;
+    private Button buttonLoadImage;
+
     private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.loadDogImage();
         viewModel.getDogImage().observe(this, new Observer<DogImage>() {
             @Override
             public void onChanged(DogImage dogImage) {
-                Log.d(TAG, dogImage.toString());
+                Glide.with(MainActivity.this)
+                        .load(dogImage.getMessage())//что загружаем
+                        .into(imageViewDog);//куда загружаем данные, где отображаем
             }
         });
+    }
+    private void initViews(){
+        imageViewDog=findViewById(R.id.imageViewDog);
+        progressBar=findViewById(R.id.progressBar);
+        buttonLoadImage=findViewById(R.id.buttonLoadImage);
     }
 }
