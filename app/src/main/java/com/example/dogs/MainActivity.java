b1b4455dd4297fe48f1658d5915cdd539b180fa4
timur200362 <kh-g-2016@mail.ru>
2023-11-18 18:25:13 +0300

    other

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL="https://dog.ceo/api/breeds/image/random";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
