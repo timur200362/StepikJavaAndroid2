1072295fb7d29044507c44668253262819adb8e2
timur200362 <kh-g-2016@mail.ru>
2023-11-25 03:06:19 +0300

    Messenger

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String BASE_URL="https://dog.ceo/api/breeds/image/random";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
